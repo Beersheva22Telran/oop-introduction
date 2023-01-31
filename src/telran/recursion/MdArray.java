bf0a28dbfb415254a28326b9602adad7279fb135
Yuragranovsky <yuragranovsky@gmail.com>
2023-01-31 15:04:56 +0200

    HW #27 MdArray recursion for non-linear tasks

--- src/telran/recursion/MdArray.java
+++ src/telran/recursion/MdArray.java
@@ -1,5 +1,9 @@
 package telran.recursion;
 
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.function.Consumer;
+
 public class MdArray<T> {
 private MdArray<T>[] array;
 private T value;
@@ -18,5 +22,42 @@ public MdArray(int[] dimensions, int firstDim, T value) {
 			array[i] = new MdArray<>(dimensions, firstDim + 1, value);
 		}
 	}
+}
+public void forEach(Consumer<T> consumer) {
+	if(array == null) {
+		consumer.accept(value);
+	} else {
+		for(MdArray<T> mdArray: array) {
+			mdArray.forEach(consumer);
+		}
+	}
+}
+public T[] toArray(T[] ar) {
+	final ArrayList<T> list = new ArrayList<>();
+	forEach(list::add);
+	return list.toArray(ar);
+	
+}
+public T getValue(int[] indexes) {
+	MdArray<T> res = getScalar(indexes);
+	return res.value;
+}
+public void setValue(int[] indexes, T value) {
+	MdArray<T> res = getScalar(indexes);
+	res.value = value;
+}
+private MdArray<T> getScalar(int[] indexes ) {
+	MdArray<T> res = this;
+	try {
+		for(int index: indexes) {
+			res = res.array[index];
+		}
+	} catch (NullPointerException e) {
+		throw new NoSuchElementException();
+	}
+	if(res.array != null) {
+		throw new IllegalStateException();
+	}
+	return res;
 }
 }
